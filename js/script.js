2e9ac21aa1e5fbaadc04a6f3629259656d4fd170
Marco-Guarnera <Darkaquon93@gmail.com>
2024-08-20 15:49:22 +0200

    Completed Exercise 02

--- js/script.js
+++ js/script.js
@@ -50,3 +50,69 @@ console.log("result:", result, "type:", typeof result);
 playerNumberElement.innerText = "Numero giocatore: " + playerNumber;
 computerNumberElement.innerText = "Numero computer: " + computerNumber;
 resultElement.innerText = result;
+
+// Esercizio 02 - Finta Login
+
+/*
+1. Recuperare gli elementi dal DOM
+2. Creare una lista di emails autorizzate
+3. Recuperare l'email dell'utente tramite il form e salvare il dato in una variabile
+4. Controllare l'email recuperata:
+    - se si trova nella lista stampare un messaggio di accesso effettuato
+    - altrimenti stampare un messaggio di accesso negato
+*/
+
+// Fase di preparazione
+
+// Recupero gli elementi dal DOM
+const formElement = document.getElementById("form");
+console.log(formElement);
+
+const emailField = document.getElementById("email");
+console.log(emailField);
+
+const messageElement = document.getElementById("message");
+console.log(messageElement);
+
+// Creo una lista di emails autorizzate
+const authorizedEmails = ["marco.guarnera@gmail.com", "mario.rossi@gmail.com"];
+console.table(authorizedEmails);
+
+// Fase di gestione eventi
+formElement.addEventListener("submit", function (event) {
+    // Blocco l'invio del form e il reloading della pagina
+    event.preventDefault();
+
+    // Fase di raccolta dati
+
+    // Recupero l'email dell'utente tramite il form e salvo il dato in una variabile
+    const userEmail = emailField.value.trim();
+    console.log(userEmail);
+
+    // Email Validation
+    if (!userEmail) return;
+
+    // Fase di elaborazione
+
+    // Variabile flag
+    let isAuthorized = false;
+
+    // Controllo l'email recuperata
+    for (let i = 0; !isAuthorized && i < authorizedEmails.length; i++) {
+        const emailToCheck = authorizedEmails[i];
+        if (userEmail === emailToCheck) {
+            isAuthorized = true;
+        }
+    }
+
+    const message = isAuthorized ? "Accesso Effettuato" : "Accesso Negato";
+
+    // Fase di produzione
+
+    // Stampo il messaggio
+    messageElement.innerText = message;
+
+    // Svuoto il form
+    formElement.reset();
+
+})
